b99773545ed06a27dea4528a6a1638a3a3cd1553
Norton Commander <c4n4d4@yandex.ru>
2015-03-23 00:21:29 +0300

    Fixes

--- grid.js
+++ grid.js
@@ -3,7 +3,7 @@ GNU GENERAL PUBLIC LICENSE, Version 2, June 1991 (license.txt)
 Copyright by @c4n4d4, @Juribiyan, 0chan.cf, nullch.org, 0chan.hk and tracks/loops authors (for rights holders: write me about your stuff to c4n4d4@yandex.ru)
 */
 
-var loops = [ 		//All loops was renamed from .ogg in *.loop (troubles with copyrights *LOL*)
+var loops = [ 		//All loops was renamed from *.ogg in *.loop (troubles with copyrights LOL)
     { url: 'chrissu_and_rawfull_panorama.loop' },
     { url: 'crystal_castles_crimewave.loop' },
     { url: 'cutline_die_for_you_shock_one_remix.loop' },
@@ -177,7 +177,7 @@ $(document).ready(function() {
     });
 });
 
-function editmode() {		//function for turn patterns edit-mode (write in F12 console "editmode();")
+function editmode() {		//function for turn patterns-edit mode (write in "F12 console": editmode();)
     $('#nullgrid').off('click');
     effect = false;
     $('#palette').slideDown();
